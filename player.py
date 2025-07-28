8de819736d7663153bad7d6f445066e26910f5ac
usrmod <82325010+usrmod@users.noreply.github.com>
2025-07-28 21:03:34 +0530

    ch2 l7 completed

--- player.py
+++ player.py
@@ -48,5 +48,14 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.rotate(dt)
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_c]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
 
 # def main():
