5ba0c8e0ce408ba36cf1215206cc08b9e3f675f2
usrmod <82325010+usrmod@users.noreply.github.com>
2025-07-30 20:28:38 +0530

    trying to figure out how to call Circleshape parent instance variable in child class asteroid method update - ch3 l1

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import *
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, PLAYER_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (x, y), PLAYER_RADIUS, 2)
+
+    def update(self, dt):
+        pass
